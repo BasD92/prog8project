ed6b9bec2a39f1fff1a50d9bb29a46b5a72b687b
BasD92 <BasD92@gmail.com>
2018-05-30 22:08:40 +0200

    Created Singleton Game class and add game loop

--- dev/game.ts
+++ dev/game.ts
@@ -1,10 +1,31 @@
 class Game {
-    constructor(){
-        console.log("new game created!")
 
-        let c = new Car()
-        c.update()
+  // Game instance
+  private static instance: Game;
+
+  public c: Car;
+
+  // Private constructor Singleton
+  private constructor() {
+    console.log("Singleton game created!");
+
+    this.c = new Car();
+
+    this.gameLoop();
+  }
+
+  private gameLoop() {
+    // Test game loop
+    this.c.update();
+
+    requestAnimationFrame(()=>this.gameLoop());
+  }
+
+  // Check if Game object exists
+  public static getInstance() {
+    if (!Game.instance) {
+      Game.instance = new Game();
     }
+    return Game.instance;
+  }
 }
-
-window.addEventListener("load", () => new Game())
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,33 @@
 "use strict";
 var Car = (function () {
     function Car() {
-        console.log("vroom!");
+        console.log("car created");
     }
+    Car.prototype.update = function () {
+        console.log("vrooom!");
+    };
     return Car;
 }());
 var Game = (function () {
     function Game() {
-        console.log("new game created!");
-        var c = new Car();
+        console.log("Singleton game created!");
+        this.c = new Car();
+        this.gameLoop();
     }
+    Game.prototype.gameLoop = function () {
+        var _this = this;
+        this.c.update();
+        requestAnimationFrame(function () { return _this.gameLoop(); });
+    };
+    Game.getInstance = function () {
+        if (!Game.instance) {
+            Game.instance = new Game();
+        }
+        return Game.instance;
+    };
     return Game;
 }());
-window.addEventListener("load", function () { return new Game(); });
+window.addEventListener("load", function () {
+    Game.getInstance();
+});
 //# sourceMappingURL=main.js.map
